968491b1260b3e9239f1ebcc84fa539aaf5a823e
Jacky <newbdez33@gmail.com>
2021-03-04 09:13:52 +0900

    basic layout done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
+import Equity from "./Equity";
+import 'antd/dist/antd.css';
 
 const reactLogo = require("./../assets/img/react_logo.svg");
 import "./../assets/scss/App.scss";
+import Title from "antd/lib/typography/Title";
 
 class App extends React.Component<Record<string, unknown>, undefined> {
   public render() {
     return (
       <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
+        <Title level={3}>Equity (USDT)</Title>
+        <Equity />
       </div>
     );
   }
---
+++ src/components/Equity.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Statistic, Row, Col, Typography, Button, Divider } from 'antd';
+const { Text } = Typography;
+
+type EquityState = {
+    data: any,
+    msg: string
+  }
+
+export default class Equity extends Component<{}, EquityState> {
+
+    state: EquityState = {
+        data:{},
+        msg:"Loading data..."
+    }
+
+    getData() {
+        fetch('./tony.json',{ headers : 
+            { 
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }).then( response => {
+            console.log(response)
+            return response.json();
+        }).then( ret => {
+            console.log(ret);
+            this.setState({
+                data: ret,
+                msg: ""
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    render() {
+        if ( this.state.msg == "" ) {
+            return  (
+                <div>
+                    <Row gutter={16}>
+                        <Col span={10}>
+                            <Statistic title="Current Equity" value={this.state.data.current_equity} precision={2} />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic title={"Profit in " + this.state.data.duration + " days"} value={this.state.data.profit} precision={2} />
+                        </Col>
+                        <Col span={6}>
+                            <Statistic title={"APR in " + this.state.data.duration + " days"} value={this.state.data.apr} suffix="%" />
+                        </Col>
+                        <Col span={10} style={{ marginTop: 16 }}>
+                            <Statistic title={"Previous Equity (" + this.state.data.previous_updated + ")"} value={this.state.data.previous_equity} precision={2} />
+                        </Col>
+                    </Row>
+                    <Divider />
+                    <Text type="secondary">Last updated: {this.state.data.updated}</Text>
+                </div>
+              );
+        }else {
+            return <p>{this.state.msg}</p>
+        }
+    }
+}
